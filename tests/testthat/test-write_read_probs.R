d1e3dbeb9410dc5d4a7c76daaa8b2047b3e73a87
Karl Broman <kbroman@gmail.com>
2017-03-20 21:33:36 -0500

    Add another test of write_probs/read_probs

--- tests/testthat/test-write_read_probs.R
+++ tests/testthat/test-write_read_probs.R
@@ -23,3 +23,26 @@ test_that("write_probs and read_probs work", {
     unlink(file)
 
 })
+
+test_that("write_probs and read_probs work for backcross", {
+
+    library(qtl2geno)
+    grav <- read_cross2(system.file("extdata", "grav2.zip", package="qtl2geno"))
+    grav <- grav[,4:5]
+    map <- insert_pseudomarkers(grav, step=1, stepwidth="max")
+    pr <- calc_genoprob(grav, map, error_prob=0.002)
+
+    file <- file.path(tempdir(), "grav.sqlite")
+
+    write_probs(file, pr, map)
+
+    pr4 <- read_probs(file, chr="4")
+    expect_equal(pr4, pr[,4])
+
+    pr5 <- read_probs(file, chr="5", pos=c(29.8, 32.8))
+    expected <- pr[,"5"]
+    expected[["5"]] <- expected[["5"]][,,40:42]
+    expect_equal(pr5, expected)
+
+    unlink(file)
+})
